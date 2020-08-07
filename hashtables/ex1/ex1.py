702853922657f056538d76bbe50269985718a6f4
Sandra Coburn <sandra_coburn@hotmail.com>
2020-08-07 10:12:08 -0700

    ex1 wip, ex2 finished

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,36 @@
-def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
+
+"""
+    - Given a package with a weight limit `limit` and a list `weights` of item
+weights
+    - find two items whose sum of weights equals the weight limit `limit`.
+    - return a tuple of integers that has the following form: (zero, one)
+    - each element represents the item weights of the two packages
+    - The higher valued index should be placed in the `zeroth` index and
+the smaller index should be placed in the `first` index.
+    - Function should return None if that condition doesn't exist
+    ex: input: weights = [ 4, 6, 10, 15, 16 ], length = 5, limit = 21
+output: [ 3, 1 ]  # since these are the indices of weights 15 and 6 whose sum equals 21
+ """
     # Your code here
+def get_indices_of_item_weights(weights, length, limit):
+    cache = {}
+    for index, num in enumerate(weights):
+        for index2, num2 in enumerate(weights):
+            if num + num2 == limit:
+                if num > num2:
+                    if index not in cache:
+                        cache[index] = index2
+                        match = index, index2
+                        return match
+                else: 
+                    cache[index2] = index
+                    match = index2, index
+                    return match
+    
 
     return None
+
+weights = [ 4, 6, 10, 15, 16 ]
+length = 5 
+limit = 21
+get_indices_of_item_weights(weights, length, limit)
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,3 +1,22 @@
+'''
+ Each ticket is represented as a class
+ - the `source` string represents the starting airport
+ - `destination` string represents the next airport
+ - The ticket for your first flight has a destination with a `source` of `NONE`
+ - the ticket for your final flight has a `source` with a `destination` of `NONE`
+tickets = [
+    Ticket{ source: "PIT", destination: "ORD" },
+    Ticket{ source: "XNA", destination: "CID" },
+    Ticket{ source: "SFO", destination: "BHM" },
+    Ticket{ source: "FLG", destination: "XNA" },
+    Ticket{ source: "NONE", destination: "LAX" },
+    Ticket{ source: "LAX", destination: "SFO" },
+    Ticket{ source: "CID", destination: "SLC" },
+    Ticket{ source: "ORD", destination: "NONE" },
+    Ticket{ source: "SLC", destination: "PIT" },
+    Ticket{ source: "BHM", destination: "FLG" }
+]
+'''
 #  Hint:  You may not need all of these.  Remove the unused functions.
 class Ticket:
     def __init__(self, source, destination):
@@ -6,9 +25,16 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    cache = {}
+    route = [None] * length
+
+    for i in range(length):
+        if tickets[i].source is "NONE":
+            route[0] = tickets[i].destination
+        if tickets[i].source not in cache:
+            cache[tickets[i].source] = tickets[i].destination
+    for j in range(length):
+        if route[j-1] is not None:
+            route[j] = cache[route[j-1]]
 
     return route
