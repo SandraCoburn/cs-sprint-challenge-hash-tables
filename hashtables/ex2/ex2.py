96e47abe8616538ec7d95fb31d8368a094c2366f
Sandra Coburn <sandra_coburn@hotmail.com>
2020-08-07 10:37:22 -0700

    exe3 finished

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -38,3 +38,18 @@ def reconstruct_trip(tickets, length):
             route[j] = cache[route[j-1]]
 
     return route
+
+
+# tickets = [
+#     Ticket{ source: "PIT", destination: "ORD"},
+#     Ticket{ source: "XNA", destination: "CID" },
+#     Ticket{ source: "SFO", destination: "BHM" },
+#     Ticket{ source: "FLG", destination: "XNA" },
+#     Ticket{ source: "NONE", destination: "LAX" },
+#     Ticket{ source: "LAX", destination: "SFO" },
+#     Ticket{ source: "CID", destination: "SLC" },
+#     Ticket{ source: "ORD", destination: "NONE" },
+#     Ticket{ source: "SLC", destination: "PIT" },
+#     Ticket{ source: "BHM", destination: "FLG" }
+# ]
+# print(reconstruct_trip(tickets))
